63ff580899ed13553fdbee924685ad9badcfb260
Marcio Vale <mvale@smartdigit.pt>
2024-12-01 12:19:02 +0000

    Add accessibility

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
   return <p>Customers Page</p>;
 }
